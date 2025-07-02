aac51d88a03bf0166e8289e706b5c560ff787312
Руслан <stla7824@gmail.com>
2025-07-02 15:22:09 +0700

    поменять docker-compose_db.yml для скриптов

--- db_script/init-topics.sh
+++ db_script/init-topics.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
-# Ждём, пока Kafka станет доступен
+set -x
+
+echo ">>> Waiting for Kafka to be ready…"
 cub kafka-ready -b localhost:9092 1 20
 
-kafka-topics \
-  --bootstrap-server localhost:9092 \
-  --create --if-not-exists \
-  --topic profile_registration \
-  --partitions 1 \
-  --replication-factor 1 # в продакшене ставить >= 3
+echo ">>> Creating topic profile_registration…"
+kafka-topics --bootstrap-server localhost:9092 \
+             --create --if-not-exists \
+             --topic profile_registration \
+             --partitions 1 \
+             --replication-factor 1
+echo ">>> Topic created!"
