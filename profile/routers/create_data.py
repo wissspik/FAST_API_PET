fd77677065e02f118d3f826c15913702e6784853
Руслан <stla7824@gmail.com>
2025-07-05 02:23:05 +0700

    изменил ручки для профиля

--- profile/routers/create_data.py
+++ profile/routers/create_data.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter,UploadFile,File,HTTPException
 from datetime import datetime, timezone
-from profile.shapes.shapes import FileUpload
+
+from starlette.responses import JSONResponse
+
+from profile.shapes.shapes import FileUpload,ChangeProfile
 from profile.utils.sql_request import get_user_id
 from profile.database.base import SessionDep
 from profile.utils.sql_request import create_photo
-
+from profile.utils.sql_request import put_data_profile
 app = APIRouter(prefix='/profile', tags=['profile'])
 
 
-@app.post('/data')
+@app.post('/upload_photo')
 async def upload_file(user_id : int,file : UploadFile = File(...)):
     check_user = await  get_user_id(user_id)
     if not check_user:
@@ -30,3 +33,15 @@ async def upload_file(user_id : int,file : UploadFile = File(...)):
 
 
     return {"status":"ok"}
+
+@app.put('/change_profile')
+async def change_profile(session:SessionDep,data : ChangeProfile):
+    check_user = await get_user_id(data['user_id'])
+    if check_user:
+        result = await put_data_profile(session,data)
+        if result:
+            return {'comment':"Пользователь успешно поменял свои данные"}
+        else:
+            raise HTTPException(status_code=400, detail="Проблема с данными,который ввёл пользователь")
+    else:
+        raise HTTPException(status_code=400, detail="Данного id не существует")
