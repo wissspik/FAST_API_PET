06eedae1703e38128ba26170540bfba7175dfe85
Руслан <stla7824@gmail.com>
2025-07-05 18:55:02 +0700

    изменил kong

--- db_script/01-create-dbs.sql
+++ db_script/01-create-dbs.sql
@@ -1,3 +1,21 @@
+--- 1)создаем базы данных ---
 CREATE DATABASE auth_db;
 CREATE DATABASE profile_db;
-CREATE DATABASE kong;
+
+--- 2)создаем роль для kong for gateway ---
+CREATE ROLE kong WITH LOGIN PASSWORD 'kong';
+
+CREATE DATABASE kong_DB OWNER kong;
+
+\connect kong_db
+
+
+-- 3) даю права на существующие объекты в public
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kong;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kong;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO kong;
+
+-- 4) делаю так, чтобы все будущие объекты в public автоматически доставались kong
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO kong;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO kong;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO kong;
