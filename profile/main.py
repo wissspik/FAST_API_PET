92a2ec95d0053d210375153216710046d045928d
Руслан <stla7824@gmail.com>
2025-07-06 18:50:37 +0700

    добавил роутер в main

--- profile/main.py
+++ profile/main.py
@@ -3,11 +3,13 @@ from profile.routers.create_data import app as create_data
 from profile.routers.db import app as db
 from starlette.middleware.cors import CORSMiddleware
 from profile.utils.kafka import lifespan
-
+from profile.routers.profile import app as profile
 
 app = FastAPI(lifespan = lifespan)
 app.include_router(create_data)
 app.include_router(db)
+
+app.include_router(profile)
 
 origins = [
     "http://localhost:3000",
--- profile/routers/profile.py
+++ profile/routers/profile.py
@@ -7,7 +7,7 @@ app = APIRouter(tags=['profile'])
 
 @app.get("/profile/{user_id}")
 async def profile(user_id = Depends(get_user_id)):
-    user = get_user_id(user_id)
+    user = await get_user_id(user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="Пользователь не найден")
     user_data = {
