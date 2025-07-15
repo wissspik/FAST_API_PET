68d6847de6ee03f61d0c7dc016ed0922bdbdc324
Руслан <stla7824@gmail.com>
2025-07-15 12:13:32 +0200

    поменял тип данных,который я помешаю в redis

--- profile_service/routers/profile.py
+++ profile_service/routers/profile.py
@@ -1,16 +1,14 @@
 import base64
-from datetime import datetime, timedelta
+from datetime import datetime,timedelta
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from profile_service.utils.sql_request import (
-    get_user_id_photo,
-    get_user_id_profile,
-    add_redis,
-    get_redis,
-    delete_redis,
-)
-from profile_service.utils.JWT import get_access_token
+
+
+from profile_service.utils.sql_request import (get_user_id_photo,
+                                               get_user_id_profile, add_redis, get_redis, delete_redis)
+from profile_service.utils.JWT import  get_access_token
+
 
 app = APIRouter(tags=["profile_service"])
 
@@ -18,7 +16,7 @@ app = APIRouter(tags=["profile_service"])
 @app.get("/profile/{user_id}")
 async def profile(user_id: int, user_id_token: int = Depends(get_access_token)):
     if user_id_token != user_id:
-        raise HTTPException(status_code=401, detail="человек не имеет прав")
+        raise  HTTPException(status_code=401,detail="человек не имеет прав")
     user = await get_user_id_profile(user_id)
     user_photo = await get_user_id_photo(user_id)
     if user is None:
@@ -37,28 +35,33 @@ async def profile(user_id: int, user_id_token: int = Depends(get_access_token)):
         "file_size": user_photo.file_size if user_photo else None,
     }
     return user_data
-
-
 @app.post("/visit_time")
-async def visit_time(user_id: int = Depends(get_access_token)):
+async def time_visit(user_id: int = Depends(get_access_token)):
     user = await get_user_id_profile(user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="Пользователь не найден")
     if await get_redis(user_id):
         await delete_redis(user_id)
     time_now = datetime.now()
-    await add_redis(user_id, time_now)
-
+    await add_redis(user_id, time_now.isoformat())
 
 @app.post("/check_time")
 async def check_time(user_id: int):
     time_now = datetime.now()
     client = await get_redis(user_id)
     if client is None:
-        await add_redis(user_id, time_now)
-        return {"user": True, "reason": "first_visit"}
-    last_visit = datetime.fromisoformat(client)
+        # Если записи нет — считаем, что пользователь заходит впервые
+        # Можно сразу записать текущее время и возвращать успех
+        await add_redis(user_id,time_now)
+
+    last_visit_str = client.decode("utf-8")
+
+    last_visit = datetime.fromisoformat(last_visit_str)
+
     delta = time_now - last_visit
+
     if delta > timedelta(minutes=5):
-        return {"status": "unactivate"}
-    return {"status": "activate"}
+        # Если прошло больше 5 минут — пользователь не в сети
+        return {"status":"unactivate"}
+    else:
+        return {"status":"activate"}
